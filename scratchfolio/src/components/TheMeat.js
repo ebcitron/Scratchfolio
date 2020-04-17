cf4078dbea4b54e556ca1f911ce2df347a6941b3
Eli Citron <ebcitron@gmail.com>
2020-04-16 20:00:19 -0500

    Getting started

---
+++ scratchfolio/src/components/TheMeat.js
@@ -0,0 +1,154 @@
+
+const theMeat = () =>{
+       return(
+			<div id="wrapper" class="fade-in">
+
+				<!-- Intro -->
+					<div id="intro" class="hidden">
+						<h1>Eli Benjamin Citron</h1>
+						<p>Introduction to my portfolio... come up with a title for myself</p>
+					</div>
+				<!-- Header -->
+					<header id="header">
+						<a href="index.html" class="logo">Welcome</a>
+					</header>
+
+				<!-- Nav -->
+					<nav id="nav">
+						
+						
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Featured Post -->
+							<article class="post featured">
+								<header class="major">
+									<span class="date">January 15th 2019</span>
+									<h2><a href="#">Who and what am I?</a></h2>
+                                    <p>Some dude from Chicago who is Fascinated with Computers and Passionate in his Craft. Here are some examples of what I can Acomplish</p>
+								</header>
+								<a href="#" class="image main"><img src="images/IMG_0012.jpg" alt=""/></a>
+								<ul class="actions special">
+									<li><a href="./generic.html" class="button large">Full Story</a></li>
+								</ul>
+							</article>
+
+						<!-- Posts -->
+							<section class="posts">
+								<article>
+									<header>
+										<span class="date">April 24, 2017</span>
+										<h2><a href="#">My favorite animal is the octopus</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/octopus.png" alt=""/></a>
+									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">Full Story</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date">April 22, 2017</span>
+										<h2><a href="#">I enjoy computer science</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/code-matrix.jpg" alt=""/></a>
+									<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis magna etiam.</p>
+									<ul class="actions special">
+										<li><a href="#" class="button">Full Story</a></li>
+									</ul>
+								</article>
+								
+							</section>
+
+						<!-- Footer -->
+							<footer>
+								<div class="pagination">
+									<!--<a href="#" class="previous">Prev</a>-->
+								</div>
+							</footer>
+
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<section>
+							<form method="post" action="#">
+								<div class="fields">
+									<div class="field">
+										<label for="name">Name</label>
+										<input type="text" name="name" id="name">
+									</div>
+									<div class="field">
+										<label for="email">Email</label>
+										<input type="text" name="email" id="email">
+									</div>
+									<div class="field">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" rows="3"></textarea>
+									</div>
+								</div>
+								<ul class="actions">
+									<li><input type="submit" value="Send Message"/></li>
+								</ul>
+							</form>
+						</section>
+						<section class="split contact">
+							<section class="alt">
+								<h3>Address</h3>
+								<p>1234 Somewhere Road #87257<br>
+								Nashville, TN 00000-0000</p>
+							</section>
+							<section>
+								<h3>Phone</h3>
+								<p><a href="#">(847)624-2432</a></p>
+							</section>
+							<section>
+								<h3>Email</h3>
+								<p><a href="#">ebcitron@gmail.com</a></p>
+							</section>
+							<section>
+								<h3>Social</h3>
+								<ul class="icons alt">
+									<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+								</ul>
+							</section>
+						</section>
+					</footer>
+
+				<!-- Copyright -->
+					<div id="copyright">
+						<ul><li>© Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
+					</div>
+
+			<div class="bg fixed" style="transform: none;"></div><a href="#navPanel" id="navPanelToggle" class="">Menu</a></div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script><div id="navPanel"><nav><ul class="links">
+							<li class="active"><a href="index.html">The Work of Eli Citron</a></li>
+							<li><a href="generic.html">Generic Page</a></li>
+							<li><a href="elements.html">Elements Reference</a></li>
+						</ul><ul class="icons alt">
+							<li><a href="#" class="icon fa-twitter alt"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-facebook alt"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-instagram alt"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon fa-github alt"><span class="label">GitHub</span></a></li>
+						</ul></nav><a href="#navPanel" class="close" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></a></div>
+
+	
+<span style="margin: 0px auto; border: 2px dotted rgb(0, 0, 0); position: absolute; z-index: 2147483647; visibility: hidden; left: 195px; width: 0px; top: 632px; height: 0px;"></span><span style="z-index: 2147483647; position: absolute; visibility: hidden; left: 180px; width: 50px; top: 617px; height: 20px; font-size: 10px; color: black;"></span>
+    </body>
+</html>
+}
+
+export default theMeat
